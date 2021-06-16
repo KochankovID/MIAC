3a47e2960419316dbe7122be4bbdbd89c84661ab
Restyled.io <commits@restyled.io>
2021-06-16 11:34:58 +0000

    Restyled by shellharden

--- miac/django-entrypoint.sh
+++ miac/django-entrypoint.sh
@@ -4,7 +4,7 @@
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
-bash ./wait-for-it.sh $db_host:$db_port
+bash ./wait-for-it.sh "$db_host:$db_port"
 
 # Apply database migrations
 echo "Apply database migrations"
