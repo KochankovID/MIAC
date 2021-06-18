f6a0b7224e65b1d11afa2e90ed681a156d9ee18c
Ilya Kochankov <ilyakochankov@yandex.ru>
2021-06-18 07:07:35 +0300

    add cors policy

--- miac/miac/settings.py
+++ miac/miac/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "phonenumber_field",
     "drf_yasg",
+    "corsheaders",
     "users",
 ]
 
@@ -45,6 +46,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "miac.urls"
